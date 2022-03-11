ac64ce7c34e274c304f2fb2d54d49af71206ae90
xiongfei <feiqi5211@gmail.com>
2022-03-11 11:59:47 +0800

    update scripts

--- inference.sh
+++ inference.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 python inference.py --task tasd \
-            --file_path 'inf.txt' \
+            --file_path ./inf.txt \
             --model_name_or_path ./pretrained-models/t5-base \
             --paradigm annotation \
             --n_gpu 0 \
@@ -9,4 +9,4 @@ python inference.py --task tasd \
             --gradient_accumulation_steps 2 \
             --eval_batch_size 16 \
             --learning_rate 3e-4 \
-            --ckpt 'outputs/tasd/rest15/annotation/cktepoch=20.ckpt'
+            --ckpt ./outputs/tasd/rest15/annotation/cktepoch=20.ckpt
